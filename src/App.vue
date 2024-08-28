720e9619a04277987ca9662e02469814792f8acf
yangyang993 <sonnenlicht@foxmail.com>
2024-08-28 18:22:17 +0800

    安装tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import Icon_svg from './components/icons/icon_svg.vue';
+import { RouterView } from 'vue-router'
+import AppHeader from './components/App-Header.vue';
 
+AppHeader
 </script>
 
 <template>
-  <Icon_svg></Icon_svg>
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-  </nav>
+  <AppHeader></AppHeader>
+
 
 
 
---
+++ src/components/App-Header.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+import IconSvg from './icons/icon_svg.vue';
+</script>
+<template>
+  <div class="w-screen h-20 bg-slate-100 flex justify-between items-center  px-5 ">
+    <div class="flex justify-between items-center">
+      <icon-svg class="text-sky-400/100 w-10 h-10"></icon-svg>
+      <h1 class="ml-3 text-[24px] font-bold">Tailwind CSS 从 0 到 Demo</h1>
+    </div>
+    <div class="flex justify-between items-center">
+      <RouterLink to="/">文档</RouterLink>
+      <RouterLink to="/about">关于</RouterLink>
+      <div>DEMO</div>
+      <div>切换模式</div>
+    </div>
+  </div>
+</template>
+<style scoped lang="less"></style>
